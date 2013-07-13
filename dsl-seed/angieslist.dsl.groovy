e351809a30d938bfcb5ac345b57b456f7ffe819f
balaji <vajjala@gmail.com>
2013-07-13 11:07:12 -0400

    Commit

---
+++ dsl-seed/angieslist.dsl.groovy
@@ -0,0 +1,73 @@
+
+import groovy.json.*
+
+// Get GitHub repo name from the parameters for this build
+def thr = Thread.currentThread()
+def build = thr?.executable
+def resolver = build.buildVariableResolver
+def projectToBuild = resolver.resolve('project')
+
+def repos = new JsonSlurper().parseText("https://api.github.com/orgs/AngiesList/repos?per_page=100".toURL().text)
+def projectWhitelist = projectToBuild
+
+repos.findAll { projectWhitelist.contains(it.name) }.each { repo ->
+    println "$repo.name $repo.url"
+    // Trunk build
+    job {
+        name "${repo.name}-master"
+        description ellipsize(repo.description, 255)
+        logRotator(60,-1,-1,20)
+        timeout(20)
+        scm {
+            git(repo.git_url, 'master')
+        }
+        configure { project ->
+            jdkNode = methodMissing('jdk', 'Sun JDK 1.6 (latest)')
+            project / jdkNode
+            project / triggers / 'com.cloudbees.jenkins.GitHubPushTrigger'(plugin:'github@1.5') / spec ()
+            project / 'properties' / 'nectar.plugins.rbac.groups.JobProxyGroupContainer'(plugin:'nectar-rbac@3.4') / groups ()
+            project / 'properties' / 'com.cloudbees.jenkins.plugins.PublicKey'(plugin:'cloudbees-public-key@1.1')
+        }
+        steps {
+            gradle('clean build')
+        }
+        publishers {
+            archiveJunit('**/build/test-results/TEST*.xml')
+        }
+    }
+
+    // Pull request build
+    // Should be similar to above, but it's probably more readable to just repeat the code.
+    job {
+        name "${repo.name}-pull-requests"
+        description ellipsize(repo.description, 255)
+        logRotator(60,-1,-1,20)
+        timeout(20)
+        scm {
+            git(repo.git_url, 'master')
+        }
+        configure { project ->
+            jdkNode = methodMissing('jdk', 'Sun JDK 1.6 (latest)')
+            project / jdkNode
+            project / triggers / 'com.cloudbees.jenkins.plugins.github__pull.PullRequestBuildTrigger'(plugin:'github-pull-request-build@1.0-beta-2') / spec ()
+            project / 'properties' / 'nectar.plugins.rbac.groups.JobProxyGroupContainer'(plugin:'nectar-rbac@3.4') / groups ()
+            project / 'properties' / 'com.cloudbees.jenkins.plugins.PublicKey'(plugin:'cloudbees-public-key@1.1')
+            project / 'properties' / 'com.cloudbees.jenkins.plugins.git.vmerge.JobPropertyImpl'(plugin:'git-validated-merge@3.6') / postBuildPushFailureHandler(class:'com.cloudbees.jenkins.plugins.git.vmerge.pbph.PushFailureIsFailure')
+            project / 'properties' / 'com.coravy.hudson.plugins.github.GithubProjectProperty'(plugin:'github@1.5') / projectUrl("${repo.html_url}/")
+        }
+
+        steps {
+            gradle('clean build')
+        }
+        publishers {
+            archiveJunit('**/build/test-results/TEST*.xml')
+        }
+    }
+}
+
+String ellipsize(String input, int maxLength) {
+  if (input == null || input.length() < maxLength) {
+    return input;
+  }
+  return input.substring(0, maxLength) + '...';
+}
---
+++ dsl-seed/run_dsl.sh
@@ -0,0 +1,2 @@
+DSL_JAR=$JOB_DSL_HOME/job-dsl-core/build/libs/job-dsl-core-*-SNAPSHOT-standalone.jar
+java -jar $DSL_JAR angieslist.dsl.groovy $@
